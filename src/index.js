83a7763748fa1497882ab658e531d7f295842432
aovalles2711 <124206685+aovalles2711@users.noreply.github.com>
2024-02-20 09:27:03 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './styles.css';
 import App from './App';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './auth/authContext';
 
 ReactDOM.render(
   <Router>
